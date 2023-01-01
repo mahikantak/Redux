c6f59bc04e95d90814bb61c4b722e4adf8277630
Mahendra Pal <33427999+mahikantak@users.noreply.github.com>
2023-01-01 14:09:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-// Action creator
-export const selectSong = song => {
-  // Return an action
-  return {
-    type: 'SONG_SELECTED',
-    payload: song
-  };
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want it That Way', duration: '1:45' }
+  ];
 };
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
